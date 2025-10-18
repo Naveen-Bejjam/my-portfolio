47bf042dd5e05f6314472d209432b965816bc15b
Naveen Bejjam <naveen@Naveens-MacBook-Pro.local>
2025-10-19 01:29:27 +0530

    corrected hi text

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -75,11 +75,11 @@ function Hero() {
             // animate={{ opacity: 1, x: 0 }}
             // transition={{ duration: 0.8, delay: 0.2 }}
           >
-            <motion.h1 className="hero-title hero-greet" 
+            <motion.h2 className="hero-greet" 
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.4 }}
-            >Hi,  I'm</motion.h1>
+            >Hi,  I'm</motion.h2>
             {/* <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
